ad5a3ce8105d2bbcef9fd024f301e4b16953314a
Chavdar Angelov <c.l.angelov@gmail.com>
2015-10-17 15:47:28 +0300

    Addded method to create Excel

--- TravelAgency.ConsoleApp/Startup.cs
+++ TravelAgency.ConsoleApp/Startup.cs
@@ -4,6 +4,10 @@
     using Data;
     using Data.Migrations;
     using Model;
+    using System.Data.OleDb;
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
 
     public class Startup
     {
@@ -11,7 +15,7 @@
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<TravelAgencyDbContext, Configuration>());
             var db = new TravelAgencyDbContext();
-
+            /*
             var transport = new Transport
             {
                 CompanyName = "Lufthansa",
@@ -21,6 +25,36 @@
             db.Transports.Add(transport);
 
             db.SaveChanges();
+            */
+            var destinations = db.Destinations.ToList();
+
+            Console.WriteLine(destinations.Count);
+
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=../../Destinations.xlsx;Extended Properties='Excel 12.0 xml;HDR=Yes';";
+
+            OleDbConnection dbConn = new OleDbConnection(connectionString);
+
+            using (dbConn)
+            {
+                dbConn.Open();
+                WritteDestinationsToExel(dbConn, destinations);
+            }   
+        }
+
+        private static void WritteDestinationsToExel(OleDbConnection dbConn, List<Destination> destinations)
+        {
+            var excelSchema = dbConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            var sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
+            Console.WriteLine("Writting data...");
+
+            foreach (var item in destinations)
+            {
+                var insertCommand = new OleDbCommand(@"INSERT INTO [" + sheetName + "] ([Country], [Distance], [LuxuryFactor]) VALUES (@country, @distance, @luxuryFactor)", dbConn);
+                insertCommand.Parameters.AddWithValue("@country", item.Country);
+                insertCommand.Parameters.AddWithValue("@distance", item.Distance);
+                insertCommand.Parameters.AddWithValue("@luxuryFactor", item.LuxuryFactor);
+                insertCommand.ExecuteNonQuery();
+            }           
         }
     }
 }

f3bd47159015513fd3afe1d967afce72cb90086f
radenkovn <radenkovn@yahoo.com>
2015-10-18 14:27:04 +0300

    Renamed the MongoDBImporter to  MongoDBGenerator and introduced a new class MongoDBImporter

--- TravelAgency.Logic/MongoDBImporter.cs
+++ TravelAgency.Logic/MongoDBImporter.cs
@@ -10,51 +10,55 @@
     using MongoDB.Driver;
     using MongoDB.Driver.Builders;
     using TravelAgency.Data;
+    using TravelAgency.Model;
 
     public class MongoDBImporter
     {
         private const string DatabaseHost = "mongodb://127.0.0.1";
         private const string DatabaseName = "Transports";
 
-        public void GenerateSampleData()
-        {
-            this.InsertData("Busabout", 0);
-            this.InsertData("Onnibus", 0);
-            this.InsertData("Alsa", 0);
-            this.InsertData("Westbus", 0);
-            this.InsertData("Megabus", 0);
-            this.InsertData("BerlinenBus", 0);
-            this.InsertData("Swebus", 0);
-            this.InsertData("Eurolines", 0);
-            this.InsertData("Megabus", 0);
-            this.InsertData("Austrian Airlines", 1);
-            this.InsertData("FlyNiki", 1);
-            this.InsertData("Aeroflot", 1);
-            this.InsertData("Lufthansa", 1);
-            this.InsertData("Wizz Air", 1);
-            this.InsertData("Ryanair", 1);
-            this.InsertData("EasyJet", 1);
-            this.InsertData("Finnair", 1);
-            this.InsertData("Princess Cruises", 2);
-            this.InsertData("Norwegian Cruise Line", 2);
-            this.InsertData("Disney Cruise Line", 2);
-            this.InsertData("Cunard", 2);
-            this.InsertData("Silversea Cruises", 2);
-            this.InsertData("Seabourn", 2);
-        }
 
-        public string GetDataJson()
+        public void ImportData(TravelAgencyDbContext dbContext)
         {
             var db = this.GetDatabase(DatabaseName, DatabaseHost);
-            var transports = db.GetCollection<BsonDocument>("Transports");
-            return transports
+            var transportsDocuments = db.GetCollection<BsonDocument>("Transports");
+            var transportsMongo = transportsDocuments
                 .FindAll()
                 .Select(x => new
                 {
                     CompanyName = x["CompanyName"].AsString,
                     Type = x["Type"].AsInt32
                 })
-                .ToJson();
+                .ToList();
+
+            var uniqueTransportNames = new HashSet<string>();
+
+            var transportsInDB = dbContext
+                .Transports
+                .Select(x => x.CompanyName)
+                .ToList();
+
+            foreach (var transport in transportsInDB)
+            {
+                uniqueTransportNames.Add(transport);
+            }
+
+            foreach (var currentTransport in transportsMongo)
+            {
+                if (!uniqueTransportNames.Contains(currentTransport.CompanyName))
+                {
+                    uniqueTransportNames.Add(currentTransport.CompanyName);
+
+                    var transportToAdd = new Transport()
+                    {
+                        CompanyName = currentTransport.CompanyName,
+                        Type = (TransportType)currentTransport.Type
+                    };
+
+                    dbContext.Transports.Add(transportToAdd);
+                }
+            }
+            dbContext.SaveChanges();
         }
 
         private MongoDatabase GetDatabase(string name, string fromHost)
@@ -62,18 +66,6 @@
             var mongoClient = new MongoClient(fromHost);
             var server = mongoClient.GetServer();
             return server.GetDatabase(name);
-        }
-
-        private void InsertData(string companyName, int type)
-        {
-            var db = this.GetDatabase(DatabaseName, DatabaseHost);
-            var logs = db.GetCollection<BsonDocument>("Transports");
-            logs.Insert(new BsonDocument
-            { 
-                { "Id", ObjectId.GenerateNewId().ToString() },
-                { "CompanyName", companyName },
-                { "Type", type }
-            });
         }
     }
 }

2f91fbf0b020d03482d813ecb968c81a60509438
Chavdar Angelov <c.l.angelov@gmail.com>
2015-10-19 11:09:38 +0300

    DatabaseModel fixed

--- TravelAgency.Model/Destination.cs
+++ TravelAgency.Model/Destination.cs
@@ -12,13 +12,18 @@
             this.excursions = new List<Excursion>();
         }
 
+        [Key]
         public int DestinationId { get; set; }
 
         [MaxLength(75)]
+        [Required]
         public string Country { get; set; }
 
+        [Required]
         public double Distance { get; set; }
 
+        [Range(1, 10)]
+        [Required]
         public int LuxuryFactor { get; set; }
 
         public virtual ICollection<Excursion> Excursions

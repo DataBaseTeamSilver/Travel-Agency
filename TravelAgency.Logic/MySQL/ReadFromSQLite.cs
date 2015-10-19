97cb2bd3b1072f3a6e1aad187d7aaaa993a4cf79
Chavdar Angelov <c.l.angelov@gmail.com>
2015-10-19 20:28:25 +0300

    improved logic

--- TravelAgency.Logic/MySQL/ReadFromSQLite.cs
+++ TravelAgency.Logic/MySQL/ReadFromSQLite.cs
@@ -8,10 +8,10 @@
     {
         public Dictionary<int?, int> GetData()
         {
-            SQLiteConnection.CreateFile("../../TravelAgency.sqlite");
+            SQLiteConnection.CreateFile("TravelAgency.sqlite");
             SQLiteConnection dataBaseConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
             dataBaseConnection.Open();
-            string sql1 = "select * from Expenses";
+            string sql1 = "SELECT * FROM Expenses";
             SQLiteCommand command1 = new SQLiteCommand(sql1, dataBaseConnection);
             SQLiteDataReader reader = command1.ExecuteReader();
             var resultExpenses = new Dictionary<int?, int>();
@@ -19,13 +19,14 @@
 
             while (reader.Read())
             {
-                var expenseId = (int?)reader["ExpenseId"];
-                var expenseAmount = (int)reader["HotelExpenses"] + (int)reader["TransportExpenses"];
+                var expenseId = reader.GetInt32(0);
+                var hotelExpense = reader.GetInt32(1);
+                var transportExpense = reader.GetInt32(2);
+          
                 if (!resultExpenses.ContainsKey(expenseId))
                 {
-                    resultExpenses.Add(expenseId, expenseAmount);
-                }
-                Console.WriteLine("ID - {0} Hotel - {1}, Transport - {2}", reader["ExpensesId"], reader["HotelExpenses"], reader["TransportExpenses"]);
+                    resultExpenses.Add(expenseId, (hotelExpense + transportExpense));
+                }             
             }
 
             return resultExpenses;

e507d225c51da023aaef16a1d1f717de51676c7c
radenkovn <radenkovn@yahoo.com>
2015-10-19 17:02:53 +0300

    Included the MySQL Importer in the solution

--- TravelAgency.Logic/MySQL/Report.cs
+++ TravelAgency.Logic/MySQL/Report.cs
@@ -3,13 +3,19 @@
     public class Report
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int Duration { get; set; }
-        public int ClientsCount { get; set; }
-        public decimal TotalIncome { get; set; }
-        public string TransportCompany { get; set; }
-        public string TransportType { get; set; }
-        public string GuideName { get; set; }
 
+        public string Name { get; set; }
+
+        public int Duration { get; set; }
+
+        public int ClientsCount { get; set; }
+
+        public decimal TotalIncome { get; set; }
+
+        public string TransportCompany { get; set; }
+
+        public string TransportType { get; set; }
+
+        public string GuideName { get; set; }
     }
 }
